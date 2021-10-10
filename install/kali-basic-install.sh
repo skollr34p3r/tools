29a2abf7339111a51ce6b7d058ea6fea99572bae
sirchsec <85844648+sirchsec@users.noreply.github.com>
2021-10-10 14:28:34 -0400

    Update kali-basic-install.sh

--- install/kali-basic-install.sh
+++ install/kali-basic-install.sh
@@ -48,6 +48,7 @@ rm /usr/share/nmap/scripts/broadcast-avahi-dos.nse
 rm /usr/share/nmap/scripts/smb-vuln-regsvc-dos.nse
 
 ## Additional Random Tools
+sudo apt install seclists curl enum4linux feroxbuster impacket-scripts nbtscan nikto nmap onesixtyone oscanner redis-tools smbclient smbmap snmp sslscan sipvicious tnscmd10g whatweb wkhtmltopdf -y
 sudo apt install gedit nfs-common python3-pip libimage-exiftool-perl -y
 #terminator dradis rdesktop brasero xdotool telnet libncurses5-dev traceroute build-essential chromium-bsu vlc mpv cifs-utils scrot filezilla libreoffice golang tree dnsutils coolkey t50 krb5-user
 
