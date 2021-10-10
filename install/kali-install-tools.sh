3f5a81e023a85f973d32cd0f76e07cbe51aa8d6f
sirchsec <85844648+sirchsec@users.noreply.github.com>
2021-10-10 12:15:27 -0400

    Update kali-install-tools.sh

--- install/kali-install-tools.sh
+++ install/kali-install-tools.sh
@@ -1,34 +1,55 @@
 #!/bin/bash
 
+### Check if root
 if [ "$EUID" -ne 0 ]
 	then echo "Please run as root"
 	exit
 fi
 
-### Upgrade prompt ###
+### Update and enable download over https
+sudo apt update
+echo "### Enabliing Secure File Download ###"
+sudo apt install apt-transport-https -y
+
+### Upgrade prompt
 read -p "Do you want to upgrade? y/n: " prompt
 if [[ "$prompt" =~ ^([yY][eE][sS]|[yY])$  ]]
 	then
     echo "#### Upgrading ####"
     sudo apt update && sudo apt upgrade -y
 else
-    echo "### Updating Only ###"
-    sudo apt update
+    echo "### Skipping Upgrade ###"
+    
 fi
 
-### More wordlists
-sudo apt install seclists -y
+### Disable sleep mode
+systemctl mask sleep.target suspend.target hibernate.target hybridsleep-target
 
-## Unzip rockyou only if zipped file exists
-#gunzip /usr/share/wordlists/rockyou.txt.gz
+### Remove default keys
+mkdir /etc/ssh/default_kali_keys
+mv /etc/ssh/ssh_host_* /etc/ssh/default_kali_keys/
+dpkg-reconfigure openssh-server
 
-##add on for enum4linux for more functionality
-sudo apt install ldapscripts -y
+### More wordlists and directiry busting tools
+sudo apt install seclists ffuf -y
 
-## Brute force tools
+## Brute force login tools
 sudo apt install brutespray -y
 
+## AD Testing Tools
+sudo apt install ldapscripts bloodhound veil -y
 
+### Install exploit compiling packages
+apt install mingw-w64 -y
+apt install g++-multilib -y
+
+## Remove dos NSE Scripts
+rm /usr/share/nmap/scripts/broadcast-avahi-dos.nse
+rm /usr/share/nmap/scripts/smb-vuln-regsvc-dos.nse
+
+## Additional Random Tools
+sudo apt install nfs-common rdesktop python3-pip libimage-exiftool-perl -y
+#terminator dradis brasero xdotool telnet libncurses5-dev traceroute build-essential chromium-bsu vlc mpv cifs-utils scrot filezilla libreoffice golang tree dnsutils coolkey t50 krb5-user
 
 #### Unknown additional tools from notes ###
 #sudo apt install python-xlrd -y
@@ -37,3 +58,7 @@ sudo apt install brutespray -y
 #sudo apt install erlang-base-hipe
 #git clone https://github.com/trustedsec/ptf
 #git clone https://github.com/sameera-madushan/Print-My-Shell
+
+#remove unused packages
+sudo apt autoclean
+sudo apt -y autoremove
