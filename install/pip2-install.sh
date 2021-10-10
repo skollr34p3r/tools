ab598fd6c000f36d7e659b0b337627ff60289fc0
sirchsec <85844648+sirchsec@users.noreply.github.com>
2021-10-10 12:20:08 -0400

    Update pip2-install.sh

--- install/pip2-install.sh
+++ install/pip2-install.sh
@@ -8,5 +8,5 @@ fi
 
 mkdir -p /opt/pip/
 curl https://bootstrap.pypa.io/pip/2.7/get-pip.py -o /opt/pip/get-pip.py
-python /opt/pip/get-pip.py
+sudo python /opt/pip/get-pip.py
 echo "Install pip2 modules with: python -m pip install <module>"
