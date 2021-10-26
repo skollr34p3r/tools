1dc7c64fb28774e635dffa993afc233d51dc5c52
sirchsec <85844648+sirchsec@users.noreply.github.com>
2021-10-25 21:10:36 -0400

    Create vscode.sh

---
+++ install/vscode.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+
+##Regular Install Stuff
+
+####  ENTER IT HERE
+####
+####
+
+
+##Fix Permissions
+sudo chown root:root /usr/share/code/chrome-sandbox && sudo chmod 4755 /usr/share/code/chrome-sandbox
